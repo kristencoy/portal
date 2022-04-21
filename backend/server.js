d52d3d8217ab76375e5443ef097cbf14f0324948
Kristen Coy <kristenbwinslow@gmail.com>
2022-04-21 11:20:29 -0400

    initial user models, routes, controllers

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 5000;
+
+// connect to db
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`server started on ${PORT}`));
